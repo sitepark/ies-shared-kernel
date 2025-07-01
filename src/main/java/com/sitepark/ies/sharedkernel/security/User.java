a65acc21ef4927688284a65fde184c2255f437b6
Holger Veltrup <veltrup@sitepark.com>
2025-07-01 10:53:52 +0200

    fix: add JsonDeserialize-Annotation for User

--- src/main/java/com/sitepark/ies/sharedkernel/security/User.java
+++ src/main/java/com/sitepark/ies/sharedkernel/security/User.java
@@ -1,11 +1,13 @@
 package com.sitepark.ies.sharedkernel.security;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 @SuppressWarnings({"PMD.AvoidFieldNameMatchingMethodName", "PMD.TooManyMethods"})
+@JsonDeserialize(builder = User.Builder.class)
 public final class User {
 
   private final String id;
