74032525c4d50c6c9206d175384f9afd5ebccc7f
Holger Veltrup <veltrup@sitepark.com>
2025-10-08 13:59:44 +0200

    refactor: getOldData to getBackwardData and deserialize lists

--- src/main/java/com/sitepark/ies/sharedkernel/audit/AuditLogService.java
+++ src/main/java/com/sitepark/ies/sharedkernel/audit/AuditLogService.java
@@ -9,9 +9,13 @@ public interface AuditLogService {
 
   List<String> getRecursiveChildIds(String parentId);
 
-  <T> Optional<T> getOldData(String auditLogId, Class<T> valueType) throws IOException;
+  <T> Optional<T> getBackwardData(String auditLogId, Class<T> valueType) throws IOException;
+
+  <T> List<T> getBackwardDataList(String auditLogId, Class<T> elementType) throws IOException;
 
   <T> T deserialize(String oldData, Class<T> valueType) throws IOException;
+
+  <T> List<T> deserializeList(String json, Class<T> elementType) throws IOException;
 
   String serialize(Object value) throws IOException;
 }
