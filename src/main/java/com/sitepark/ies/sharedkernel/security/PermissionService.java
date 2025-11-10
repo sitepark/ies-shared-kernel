427622e287cfe8ed5207ce0e979336ce89686382
Holger Veltrup <veltrup@sitepark.com>
2025-11-10 10:30:03 +0100

    refactore: PermissionService interface

--- src/main/java/com/sitepark/ies/sharedkernel/security/PermissionService.java
+++ src/main/java/com/sitepark/ies/sharedkernel/security/PermissionService.java
@@ -1,37 +1,14 @@
 package com.sitepark.ies.sharedkernel.security;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import java.util.List;
 
 public interface PermissionService {
 
   /**
-   * @throws PermissionParseException Thrown when the permission data could not be parsed.
    * @throws PermissionCreateException Thrown when the permission could not be created.
    */
-  default Permission createPermission(String permissionType) {
-    return this.createPermission(permissionType, null);
-  }
-
-  /**
-   * @throws PermissionParseException Thrown when the permission data could not be parsed.
-   * @throws PermissionCreateException Thrown when the permission could not be created.
-   */
-  Permission createPermission(String permissionType, String permissionData);
-
-  /**
-   * Expects a JSON array of the form: <code>
-   *   [
-   *    {
-   *      "type" : "TYPE",
-   *      "data" : { ... }
-   *    }
-   *   ]
-   * </code>
-   *
-   * @throws PermissionParseException Thrown when the permission data could not be parsed.
-   * @throws PermissionCreateException Thrown when the permission could not be created.
-   */
-  List<Permission> createPermissions(String permissions);
+  Permission createPermission(JsonNode permissionData);
 
   /**
    * @throws PermissionSerializeException Thrown when the permission could not be serialized.
